473bd06f9f4c1d31aded920bdacbefeaaf48bc97
Dennis Lustre <dlustre@uci.edu>
2023-10-01 00:48:10 -0700

    Reorganize components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
-// import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from './components/Header'
+import Hero from './components/Hero'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <main>
-    </main>
-  )
+    <div>
+      <Head>
+        <title>Dennis Lustre</title>
+        <link rel="icon" href="/react-portfolio/favicon.ico" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
+      </Head>
+
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
