eac6f215515ee3217aebff7430a7decb86c430d8
Dennis Lustre <dlustre@uci.edu>
2023-09-30 22:45:59 -0700

    Set document titles

--- pages/components/Contact.tsx
+++ pages/components/Contact.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import config from "../index.json";
 import ContactForm from "./ContactForm";
+import Head from 'next/head';
 
 const Contact = () => {
   const contact = config.contact;
   return (
     <div id={contact.title} className="px-8 sm:px-12 md:px-32 pb-32 flex justify-center align-center flex-col bg-gradient-to-r from-green-300 via-teal-500 to-violet-500">
+      <Head>
+        <title>Contact</title>
+      </Head>
       <h2 className="mt-14 uppercase font-bold text-center text-white text-bold text-4xl">{contact.title}</h2>
       <ContactForm />
     </div>
