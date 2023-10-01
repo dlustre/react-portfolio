d1dccd41bb99eb7629b096f634d6b962b6097b01
Dennis Lustre <dlustre@uci.edu>
2023-09-30 20:08:14 -0700

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,9 @@ export default function Document() {
   return (
     <Html lang="en" className='scroll-smooth'>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
-      <Head />
+      <Head>
+        <link rel="icon" href="/react-portfolio/favicon.ico" />
+      </Head>
       <body className='text-black'>
         <NextScript />
         <Header />
